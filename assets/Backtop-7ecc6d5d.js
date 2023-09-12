58c029fc4e1e519d05a85d47fb8013f6aaf255a4
xiejay97 <xiejay97@users.noreply.github.com>
2023-09-12 09:11:38 +0000

    deploy: laser-ui/laser-ui@33e540e0ba7de5f3c165349fd8ce1c9384d5c77e

--- assets/Backtop-7ecc6d5d.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-a37eaf59.js";import{F as i}from"./Fab-397bfd86.js";import"./scrollTo-8963f565.js";import"./toPx-9fa97842.js";import"./add-ba109d47.js";function p(){return t.jsx("div",{style:{height:"500vh"},children:t.jsx(i,{style:{position:"fixed",right:40,bottom:40,zIndex:910},children:t.jsx(i.Backtop,{shape:"circle"})})})}export{p as default};
--- assets/Basic-8c7a3a48.js
+++
@@ -1,1 +0,0 @@
-import{j as t,B as i}from"./index-a37eaf59.js";import{A as o}from"./Affix-62325382.js";import"./getOffsetToRoot-9adf1469.js";import"./toPx-9fa97842.js";function x(){return t.jsx("div",{style:{height:"500vh",padding:"100px 0 0 40px"},children:t.jsx(o,{top:20,children:t.jsx(i,{children:"Top Affix"})})})}export{x as default};
