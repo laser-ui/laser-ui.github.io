979c92fd48eaf489c79482f25bcc3e13770c2293
xiejay97 <xiejay97@users.noreply.github.com>
2024-02-29 05:40:55 +0000

    deploy: laser-ui/laser-ui@d796181514dcf19eed07ebe40aca197a7fb70c48

---
+++ assets/Backtop-160a6cc6.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-19645604.js";import{F as i}from"./Fab-9c5dc23a.js";import"./scrollTo-1daecea7.js";import"./toPx-262bc02c.js";import"./add-67c3067d.js";function p(){return t.jsx("div",{style:{height:"500vh"},children:t.jsx(i,{style:{position:"fixed",right:40,bottom:40,zIndex:910},children:t.jsx(i.Backtop,{shape:"circle"})})})}export{p as default};
