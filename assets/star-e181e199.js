ff2e803db15992c7c521da549fcd437fa6e6d508
xiejay97 <xiejay97@users.noreply.github.com>
2024-04-26 06:15:17 +0000

    deploy: laser-ui/laser-ui@f601b92f5f145bb94db8dd360b381ba0d55e05cc

---
+++ assets/star-e181e199.js
@@ -0,0 +1,1 @@
+import{r as t}from"./index-03cf988e.js";const a=e=>t.createElement("svg",{xmlns:"http://www.w3.org/2000/svg",width:24,height:24,viewBox:"0 0 24 24",...e},t.createElement("path",{d:"M12 17.27 18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"}));export{a as S};
