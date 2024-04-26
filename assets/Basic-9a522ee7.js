74cb1849c38749d268d491e23557bcf3a5708cb7
xiejay97 <xiejay97@users.noreply.github.com>
2024-04-26 05:45:12 +0000

    deploy: laser-ui/laser-ui@b676480db5dfa19699a4c6307b0870bbfba3d042

---
+++ assets/Basic-9a522ee7.js
@@ -0,0 +1,1 @@
+import{j as t,B as i}from"./index-6708e195.js";import{A as o}from"./Affix-9d461df7.js";import"./getOffsetToRoot-ce6fc65c.js";import"./toPx-f3759d9f.js";function x(){return t.jsx("div",{style:{height:"500vh",padding:"100px 0 0 40px"},children:t.jsx(o,{top:20,children:t.jsx(i,{children:"Top Affix"})})})}export{x as default};
