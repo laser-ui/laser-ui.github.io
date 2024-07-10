2991c5d715bc8e8c9706470382bfc5e43b0ffbf4
xiejay97 <xiejay97@users.noreply.github.com>
2024-07-10 10:58:22 +0000

    deploy: laser-ui/laser-ui@a9765f429b263e6f0183e2b37ba21697846ac014

--- assets/Backtop-138a3d5a.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-fbc62ce1.js";import{F as i}from"./Fab-ffd2665b.js";import"./scrollTo-501ec74f.js";import"./toPx-6daf3b1b.js";import"./add-ece72f44.js";function p(){return t.jsx("div",{style:{height:"500vh"},children:t.jsx(i,{style:{position:"fixed",right:40,bottom:40,zIndex:910},children:t.jsx(i.Backtop,{shape:"circle"})})})}export{p as default};
---
+++ assets/Route-b0db79d6.js
@@ -0,0 +1,1 @@
+import{a1 as t,a2 as r,r as o}from"./index-a3c33b7e.js";import{M as s}from"./MdRoute-6079b419.js";function g(a){const e=t(...r.language);return o.createElement(s,a[e.value])}export{g as R};
