e07f64859984d4479eb8f2c715c7dec9d503c4ff
xiejay97 <xiejay97@users.noreply.github.com>
2024-12-19 05:57:35 +0000

    deploy: laser-ui/laser-ui@b6947acf60a6162bb8dece6e9de0b1efd40b95f9

---
+++ assets/Basic-d810ee9f.js
@@ -0,0 +1,1 @@
+import{j as t,B as o}from"./index-2a8bb1b1.js";import{A as r}from"./Affix-7e03f210.js";import"./getOffsetToRoot-7eb01cba.js";import"./toPx-9a561eb2.js";function n(){return t.jsx("div",{style:{height:"500vh",padding:"100px 0 0 40px"},children:t.jsx(r,{top:20,children:i=>t.jsx(o,{...i,children:"Top Affix"})})})}export{n as default};
---
+++ assets/setRef-a734977e.js
@@ -0,0 +1,1 @@
+import{l as r}from"./index-2a8bb1b1.js";function l(t,n){return r.isFunction(t)?(t(n),()=>{t(null)}):t?(t.current=n,()=>{t.current=null}):()=>{}}export{l as s};
