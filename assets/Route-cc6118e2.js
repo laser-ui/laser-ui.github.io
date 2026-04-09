a44f0604bee9f23eaf15ef9927f42c91842d3705
xiejay97 <xiejay97@users.noreply.github.com>
2026-04-09 06:45:08 +0000

    deploy: laser-ui/laser-ui@44becbe9fcc81f5dab37ecf828d2fba9f25b1db6

---
+++ assets/Route-cc6118e2.js
@@ -0,0 +1,1 @@
+import{a5 as t,a6 as e,j as r}from"./index-6e563557.js";import{M as s}from"./MdRoute-17dc952b.js";function g(a){const o=t(...e.language);return r.jsx(s,{...a[o.value]})}export{g as R};
